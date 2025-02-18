54cdaf012686fd55896ac07e5032e6c92665ae43
Mahathi Gumudavelli <101474281+mgjslearn@users.noreply.github.com>
2025-02-18 16:47:14 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/3DJS_project/"  // Set this to your GitHub repo name
   plugins: [react()],
   assetsInclude: ['**/*.glb']
 })
