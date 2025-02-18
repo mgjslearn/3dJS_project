0aa032e37e119e5de2eae9b05f1623106e4dab6d
mgjslearn <mahag.student@gmail.com>
2025-02-18 16:05:14 -0500

    3d portfolio

---
+++ src/models/Bird.jsx
@@ -0,0 +1,45 @@
+import {useRef, useEffect} from 'react'
+
+import birdScene from '../assets3/3d/bird.glb';
+import { useAnimations, useGLTF } from '@react-three/drei';
+import { useFrame, useThree } from "@react-three/fiber";
+
+const Bird = () => {
+    const birdRef = useRef();   
+    const {scene, animations} = useGLTF(birdScene);
+    const {actions}  = useAnimations(animations, birdRef);
+
+
+    useEffect(() => {
+        actions['Take 001'].play();
+
+    }, []);
+
+    useFrame(({clock, camera}) => {
+        // update the Y pos to move in a sin wave
+        birdRef.current.position.y = Math.sin(clock.elapsedTime) * 0.2 + 2;
+
+        if(birdRef.current.position.x > camera.position.x + 10) {
+            birdRef.current.rotation.y = Math.PI;
+        } else if(birdRef.current.position.x < camera.position.x - 10 ) {
+            birdRef.current.rotation.y = 0;
+        }
+        if(birdRef.current.rotation.y === 0) {
+            birdRef.current.position.x += 0.01;
+            birdRef.current.position.z -= 0.01;
+        } else {
+            birdRef.current.position.x -= 0.01;
+            birdRef.current.position.z += 0.01;
+        }
+    })
+
+    return (
+        // to create and display 3D objects
+        <mesh ref={birdRef} position={[-5, 2, 1]} scale={[0.003, 0.003, 0.003]}>
+          // use the primitive element when you want to directly embed a complex 3D
+          model or scene
+          <primitive object={scene} />
+        </mesh>
+      );
+    }
+export default Bird
